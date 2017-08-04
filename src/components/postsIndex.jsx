465ae63ed207954dabdf2e48438eda42af531d31
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2017-08-04 23:54:55 +0200

    Render a list of posts

--- src/components/postsIndex.jsx
+++ src/components/postsIndex.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    // Input an object (no map function) 
+    // Output an array for React to render a list of Posts
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        Posts PostsIndex
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default PostsIndex;
+function mapStateToProps(props) {
+  return { posts: props.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
