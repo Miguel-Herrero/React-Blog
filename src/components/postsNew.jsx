86430dc5697a65285cc8d255a09825a08321fcc5
Miguel-Herrero <miguel.herrerobaena@gmail.com>
2017-08-06 20:59:05 +0200

    Create a post

--- src/components/postsNew.jsx
+++ src/components/postsNew.jsx
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
+  onSubmit(values) {
+    // this === component
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      // Send the form to reduxForm, and then our onSubmit function with the context.
+      // We need to bind this because we're passing this.onSubmit as a callback function 
+      // that will be executed in some other different context outside of our component.
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -33,11 +54,37 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // Validate the inputs
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter a category";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  // If errors is empty, the form is OK to submit
+  // If errors are *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew) // Connect the redux action with our react component, returns react component
+);
